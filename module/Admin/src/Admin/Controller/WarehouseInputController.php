1fe402289cbabc657343bdfd0964c6b4a30a8cdc
nhantd292 <nhantd.292@gmail.com>
2026-05-20 21:54:30 +0700

    huy, hoàn thành phiếu nhập hàng

--- module/Admin/src/Admin/Controller/WarehouseInputController.php
+++ module/Admin/src/Admin/Controller/WarehouseInputController.php
@@ -267,18 +267,9 @@ class WarehouseInputController extends ActionController{
 
                     # cập nhật tồn kho cho sản phẩm.
                     $products_detail = $this->getServiceLocator()->get('Admin\Model\WarehouseInputDetailTable')->listItem(array('warehouse_input_id' => $id), array('task' => 'list-ajax'));
-                    foreach ($products_detail as $detail_item) {
-                        // cập nhật số lượng hàng trả cho bảng contract detail
-                        $contract_detail_item_update = $this->getServiceLocator()->get('Admin\Model\ContractDetailTable')->getItem(array('id' => $detail_item['orders_detail_id']));
-                        $number_return_new = $contract_detail_item_update['numbers_return'] + $detail_item['quantity'];
-                        $this->getServiceLocator()->get('Admin\Model\ContractDetailTable')->saveItem(array('data' => array('id' => $detail_item['orders_detail_id'], 'numbers_return' => $number_return_new)), array('task' => 'update-number'));
 
-                        if ($detail_item['quantity'] > $detail_item['contract_detail_quantity']) {
-                            $this->flashMessenger()->addErrorMessage('Số lượng sản phẩm "'.$detail_item['products_name'].'" trả lại nhiều hơn số lượng đã mua, số lượng mua là "'.$detail_item['contract_detail_quantity'].'" !');
-                            $this->goRoute(array('action' => 'detail', 'id' => $id));
-                            return false;
-                        }
-                        $inventory = $this->getServiceLocator()->get('Admin\Model\ProductsInventoryTable')->getItem(array('products_id' => $detail_item->product_id, 'warehouse_id' => $item->inventory_id), array('task' => 'filter'));
+                    foreach ($products_detail as $detail_item) {
+                        $inventory = $this->getServiceLocator()->get('Admin\Model\ProductsInventoryTable')->getItem(array('products_id' => $detail_item->products_id, 'warehouse_id' => $item->inventory_id), array('task' => 'filter'));
                         $quantity_new = $inventory->quantity + $detail_item->quantity;
                         $this->getServiceLocator()->get('Admin\Model\ProductsInventoryTable')->saveItem(array('data' => array('quantity' => $quantity_new, 'id' => $inventory->id)), array('task' => 'edit-item'));
                     }
@@ -292,11 +283,10 @@ class WarehouseInputController extends ActionController{
                     $this->getServiceLocator()->get('Admin\Model\CustomerDebtTable')->saveItem(array('data' => $data_debt, 'item' => $debt_item_old), array('task' => 'edit-item'));
 
                     $connection->commit();
-                    $this->flashMessenger()->addSuccessMessage('Phiếu trả hàng đã được hoàn thành!');
+                    $this->flashMessenger()->addSuccessMessage('Phiếu nhập hàng đã được hoàn thành!');
                 }
 
-
-                $item = $this->getTable()->getItem(array('id' => $id));
+                $item = $this->getTable()->getItem(array('id' => $id), array('task' => 'full'));
             }
         }
 
