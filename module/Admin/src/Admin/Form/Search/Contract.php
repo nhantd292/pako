2ec902d7783c239bfdbde4a2cd0ad56ece8a6c14
nhantd292 <nhantd.292@gmail.com>
2026-06-19 10:40:54 +0700

    thêm bộ lọc ngày xuất hóa đơn

--- module/Admin/src/Admin/Form/Search/Contract.php
+++ module/Admin/src/Admin/Form/Search/Contract.php
@@ -466,7 +466,7 @@ class Contract extends Form{
             ),
             'options' => array(
 //                'empty_option'	=> '- Ngày lên đơn -',
-                'value_options' => array('date' => 'Ngày lên đơn', 'shipped_date' => 'Ngày xuất kho', 'not_shipped' => 'Chưa có ngày xuất kho'),
+                'value_options' => array('date' => 'Ngày lên đơn', 'shipped_date' => 'Ngày xuất kho', 'not_shipped' => 'Chưa có ngày xuất kho', 'date_invoice' => 'Ngày xuất hóa đơn'),
             )
         ));
 
